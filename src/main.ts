8087c1d5ae3377ea3a77cba10084840dc27a984a
Leonardo Soares <leonardo.rock22@hotmail.com>
2024-03-27 19:17:18 -0300

    prime vue config

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,7 @@ import './style.css'
 import App from './App.vue'
 import router from './router';
 import pinia from './store/pinia';
+import PrimeVue from 'primevue/config';
+import 'primevue/resources/themes/aura-light-green/theme.css'
 
-createApp(App).use(router).use(pinia).mount('#app')
+createApp(App).use(router).use(pinia).use(PrimeVue).mount('#app')
