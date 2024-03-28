9ef5c025ac3dc18e366420c7c5a8edb5d393f441
Lusques <lucascarlosjobs@gmail.com>
2024-03-28 20:03:01 -0300

    WIP

---
+++ src/components/SideBar.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+import { ref } from "vue";
+
+import Button from 'primevue/button';
+import Sidebar from 'primevue/sidebar';
+
+const visible = ref(true);
+</script>
+
+<template>
+  <div class="card flex justify-content-center">
+      <Sidebar v-model:visible="visible" header="Sidebar">
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      </Sidebar>
+      <Button icon="pi pi-arrow-right" @click="visible = true" />
+  </div>  
+</template>
