173b195df4f7b0baf9d69fd0299a79b3a59fbd14
Lusques <lucascarlosjobs@gmail.com>
2024-03-29 00:00:30 -0300

    wip

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -10,7 +10,7 @@ const visible = ref(true);
 <template>
   <div class="card flex justify-content-center">
       <Sidebar v-model:visible="visible" header="Sidebar">
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
       </Sidebar>
       <Button icon="pi pi-arrow-right" @click="visible = true" />
   </div>  
