60b5de1a7439567c428a8f225cec5365099a7b54
Lusques <lucascarlosjobs@gmail.com>
2024-04-08 18:45:30 -0300

    change menu button icon

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -13,6 +13,6 @@ const visible = ref(false);
         <Sidebar v-model:visible="visible" header="Sidebar">
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
         </Sidebar>
-        <Button icon="pi pi-arrow-right" @click="visible = true" />
+        <Button icon="pi pi-bars" @click="visible = true" />
     </div>
 </template>
